6cae60f7c430d73eb36b5198b95e4668f148ea83
venkata <venky.pusarla47@gmail.com>
2019-01-24 22:57:04 +0530

    recdoing

--- src/main/java/com/decathlon/eureka/DecathlonEurekaServerApplication.java
+++ src/main/java/com/decathlon/eureka/DecathlonEurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.decathloneureka;
+package com.decathlon.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
